7939bac4bf522361fda450b8f5882d6f23557a86
HarkuLi <harku85697708@hotmail.com>
2023-09-17 01:47:44 +0800

    Change the project name to "evpn-vxlan-frr-3sites"

--- PC-interfaces.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-sudo docker exec -d clab-frr01-PC1 ip link set eth1 up
-sudo docker exec -d clab-frr01-PC1 ip addr add 10.10.10.1/24 dev eth1
-
-sudo docker exec -d clab-frr01-PC2 ip link set eth1 up
-sudo docker exec -d clab-frr01-PC2 ip addr add 10.10.10.2/24 dev eth1
-
-sudo docker exec -d clab-frr01-PC3 ip link set eth1 up
-sudo docker exec -d clab-frr01-PC3 ip addr add 10.10.10.3/24 dev eth1
